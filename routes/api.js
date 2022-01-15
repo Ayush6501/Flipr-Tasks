2d270ec8640c17687aafc4170b0f4945596cbde0
Ayush6501 <ayushmajumdar6501@gmail.com>
2022-01-15 23:14:52 +0530

    Flipr Task2 Completed

--- routes/api.js
+++ routes/api.js
@@ -7,38 +7,39 @@ const NodeGeocoder = require('node-geocoder');
 
 const db = mongoose.connection
 
-router.get('/', async (req, res) => {
-    let devices = [];
-    let deviceLocation = []
-
-    try {
-        const id = req.params.id
-
-        const devicesCollection = db.collection('devices')
-        const statusCollection = db.collection('status')
-        const data = await devicesCollection.find().sort({createdAt: -1}).limit(30).toArray()
-        data?.forEach((info) => devices.push(info.imei))
-
-        // devices.forEach((device) => {
-        //     const location = statusCollection.find({imei: device}).limit(50).toArray()
-        //     console.log(location)
-        // })
-
-        console.log(devices)
-        console.log(devices[0])
-        console.log(typeof (devices[0]))
-
-        for (let i = 0; i < devices.length; i++) {
-            let location = await statusCollection.find({imei: devices[0]}).limit(50).toArray()
-            console.log(location)
-            deviceLocation.push(location)
-        }
-
-        console.log(deviceLocation)
-        res.json(deviceLocation)
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+// router.get('/', async (req, res) => {
+//     let devices = [];
+//     let deviceLocation = []
+//
+//     try {
+//         const id = req.params.id
+//
+//         const devicesCollection = db.collection('devices')
+//         const statusCollection = db.collection('status')
+//         const data = await devicesCollection.find().sort({createdAt: -1}).limit(30).toArray()
+//         data?.forEach((info) => devices.push(info.imei))
+//
+//         // devices.forEach((device) => {
+//         //     const location = statusCollection.find({imei: device}).limit(50).toArray()
+//         //     console.log(location)
+//         // })
+//
+//         console.log(devices)
+//         console.log(devices[0])
+//         console.log(typeof (devices[0]))
+//
+//         for (let i = 0; i < devices.length; i++) {
+//             let location = await statusCollection.find({imei: devices[0]}).limit(50).toArray()
+//             console.log(location)
+//             deviceLocation.push(location)
+//         }
+//
+//         console.log(deviceLocation)
+//         res.json(deviceLocation)
+//     } catch (err) {
+//         res.status(500).json({ message: err.message });
+//     }
+// })
 
 router.get('/', async (req, res) => {
     try {
